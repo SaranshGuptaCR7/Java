f85a56c35e1f765fb76dc2d5ff8f3f1a68a6400d
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-09-10 19:36:33 +0530

    First java file intro only

---
+++ display.java
@@ -0,0 +1,5 @@
+class Son{
+    public static void Java(String[] args){
+    system.out.println("Hello Son! I m learning a new language Java. Not Java Script only JAVA.So DID U UNDERSTAAND??"):
+    }
+}
