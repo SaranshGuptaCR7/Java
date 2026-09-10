6b210066ebdd42e24da7bb3ac81f48bd5edaaf50
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-09-10 20:06:43 +0530

    addition after class

---
+++ addition.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void(String[] args){
+        int a = 911;
+        int b = 711;
+        int sum = a + b;
+        System.out.println("The sum of the numbers is:", + sum);
+    }
+}
