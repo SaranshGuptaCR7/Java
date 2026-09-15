8590b22eeb240ba719c98faeba418870c28fe67d
SaranshGuptaCR7 <saransh14.gupta@gmail.com>
2026-09-15 19:58:30 +0530

    data types and operators in java

---
+++ maggie recipe.java
@@ -0,0 +1,19 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("My Maggi Recipe");
+        Maggie();
+    }
+
+    public static void Maggi(){
+        System.out.println("1. Fill the utensil with water in which u will make your cheap version of ramen")
+        System.out.println("2. Put the maggi inside the utensil either put it by breaking it into two or put directly or just throw it inside")
+        System.out.println("3. Start the stove or if u dont have then go to africa in Sahara dessert put the utensil on the sand")
+        System.out.println("4. Boil the maggie for 7 minutes because Thala for a reason Son!")
+        System.out.println("5. Put the cheap ramen masala that u get with the maggie packet")
+        System.out.println("6. Add bhao bhaji masala also")
+        System.out.println("7. Add Peri-peri masala")
+        System.out.println("8. Add Cheese")
+        System.out.println("9. Then it ready to serve and put it on a plate")
+        System.out.println("10. Then throw it on the visitors face or manerfully serve it on a plate or his hands")
+    }
+}
